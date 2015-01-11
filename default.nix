f952665ae970f34464f71c183d045c290f266e66
Stephen Diehl <stephen.m.diehl@gmail.com>
2015-01-11 05:45:02 -0500

    bump nix hash

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@
 cabal.mkDerivation (self: {
   pname = "llvm-pp";
   version = "0.1.0.0";
-  sha256 = "7fc2772159097ce75a07a4cffbb81270c974a5c2ef5789439a63f8dd641379c9";
-  buildDepends = [ ansiWlPprint llvmGeneral llvmGeneralPure ];
+  sha256 = "2d8506bf9bf895f3f378d3e35575e5c33b46654f9e7fd4ec25b52c9863a4df01"
+  buildDepends = [ llvmGeneralPure ];
   meta = {
     homepage = "https://github.com/sdiehl/llvm-pp";
     description = "Pretty printer for LLVM IR";
